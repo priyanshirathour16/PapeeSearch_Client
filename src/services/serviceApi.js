d34c6d50b02fdae00fd7495036afa6c456d85570
priyanshi rathour <priyanshirathour@gmail.com>
2025-12-11 21:19:07 +0530

    change db url

--- src/services/serviceApi.js
+++ src/services/serviceApi.js
@@ -1,2 +1,5 @@
-export const ImageURl = 'http://localhost:5000/uploads/';
-export const scriptUrl = 'http://localhost:5000/'
+// export const ImageURl = 'http://localhost:5000/uploads/';
+// export const scriptUrl = 'http://localhost:5000/'
+
+export const ImageURl = 'https://papersearchbackend-1.onrender.com/uploads/';
+export const scriptUrl = 'https://papersearchbackend-1.onrender.com/'
