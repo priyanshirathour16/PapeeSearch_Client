e21838199deb49046af6af10b19f27f7babd6a08
priyanshi rathour <priyanshirathour@gmail.com>
2025-12-20 13:11:01 +0530

    add backend url

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
-    // baseURL: 'https://papersearchbackend-1.onrender.com/api',
+    // baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://rapidcollaborate.in/elkjournals_backend/api',
 });
 
 api.interceptors.request.use(
